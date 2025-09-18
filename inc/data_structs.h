eb90d4cab960357303ad01007a0c9aa796a86977
adarsh <adarshh911@gmail.com>
2025-09-18 22:31:39 +0530

    minor changes

--- inc/data_structs.h
+++ inc/data_structs.h
@@ -18,7 +18,7 @@ typedef struct {
 } RawKP;
 
 typedef struct {
-  uint64_t descriptor[4]; // 256 bits
+  uint64_t descriptor[4];
 } BriefDescriptor;
 
 typedef struct {
@@ -29,7 +29,7 @@ typedef struct {
 typedef struct {
   int idx1, idx2;
   int distance;
-  float confidence;  // Match quality (0-1)
+  float confidence
 } Match;
 
 typedef struct {
